1e632e1292980872361ad3f3ecd6415b1e94ea97
im-a-robo <nmdasil514@pccsk12.com>
2021-06-03 08:45:04 -0400

    playing around with more video stuff

--- video_control.py
+++ video_control.py
@@ -4,7 +4,29 @@ import numpy as np
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
-  
+
+cv2.namedWindow("Trackbars")
+
+def nothing(x):
+    pass  
+f = open("color.txt", "r")
+
+l_h = f.readline()
+l_s = f.readline()
+l_v = f.readline()
+u_h = f.readline()
+u_s = f.readline()
+u_v = f.readline() 
+
+f.close()
+
+cv2.createTrackbar("L-H", "Trackbars", int(l_h), 179, nothing)
+cv2.createTrackbar("L-s", "Trackbars", int(l_s), 255, nothing)
+cv2.createTrackbar("L-v", "Trackbars", int(l_v), 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", int(u_h), 179, nothing)
+cv2.createTrackbar("U-S", "Trackbars", int(u_s), 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", int(u_v), 255, nothing)
+
 while(True):
       
     # Capture the video frame
@@ -12,9 +34,16 @@ while(True):
     ret, frame = vid.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
   
+    l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L-s", "Trackbars")
+    l_v = cv2.getTrackbarPos("L-v", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U-V", "Trackbars")
+
     # Color masking
-    low_red = np.array([161, 155, 84])
-    high_red = np.array([179, 255, 255])
+    low_red = np.array([l_h, l_s, l_v])
+    high_red = np.array([u_h, u_s, u_v])
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
     red = cv2.bitwise_and(frame, frame, mask = red_mask)
 
@@ -25,6 +54,10 @@ while(True):
     # if escape is pressed break the loop
     if cv2.waitKey(1) == 27:
         break
+
+f = open("color.txt", "w")
+f.write("{}\n{}\n{}\n{}\n{}\n{}".format(str(l_h), str(l_s), str(l_v), str(u_h), str(u_s), str(u_v)))
+f.close()
 
 # After the loop release the cap object
 vid.release()
